ec2d75bd778d9a9ae69235cb1aad02b92a2c9df0
olaoguntam9@gmail.com <Olaoguntam9@gmail.com>
2022-09-06 02:05:34 +0100

    New tutorials added

---
+++ Variables/object_methods.py
@@ -0,0 +1,4 @@
+course = "Python Python tutorials"
+print(course.capitalize()) 
+print(course.replace('Python', 'Boss'))
+print(course.find('tutorials'))
---
+++ Variables/simple_calc.py
@@ -0,0 +1,5 @@
+first = float(input("Enter First Number: "))
+second = float(input("Enter Second Number: "))
+
+sum = first + second 
+print("The sum is ", sum)
