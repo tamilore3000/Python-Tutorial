313920b6706ea1ca77ca9a5c328eeb395fdeb6bb
olaoguntam9@gmail.com <Olaoguntam9@gmail.com>
2022-09-06 03:47:25 +0100

    Used Pycharm to update

--- Variables/bmi_variables.py
+++ Variables/bmi_variables.py
@@ -1,6 +1,7 @@
 # BMI CALCULATOR IN PYTHON
+import os
 from datetime import datetime
-import os , time 
+
 
 # define our clear function
 def clear():
@@ -9,27 +10,32 @@ def clear():
         os.system('cls')
     # for mac and linux(here, os.name is 'posix')
     else:
-        _ = system('clear')
+        _ = os.system('clear')
+
 
 def weight_converter(w):
+    global converted
     weight_unit = input("What is the weight unit Kgs or Lbs: ")
     if weight_unit.upper() == "KG":
         converted = w / 1  
         print("weight in kg is: ", converted)
     elif weight_unit.upper() == "LBS":
-        converted= w / 2.2
+        converted = w / 2.2
         print("weight in kg is: ", converted)
     return converted
 
+
 def height_converter(w):
+    global converted
     height_unit = input("What is the weight unit meters or feet: ")
     if height_unit.upper() == "METERS":
         converted = w / 1  
         print("height in meters is: ", converted)
     elif height_unit.upper() == "FEET":
-        converted= w / 3.281
+        converted = w / 3.281
         print("height in meters is: ", converted)
     return converted
+
 
 while True:
     try:
@@ -40,18 +46,18 @@ while True:
         weight = float(weight)
         wconverted = weight_converter(weight)
       
-        height= input("What is your height: ")
+        height = input("What is your height: ")
         height = float(height)
         hconverted = height_converter(height)
 
         break
     except ValueError:
-        #os.system(clock_settime)
+        # os.system(clock_settime)
         print("No valid integer! Please try again ...")
         clear()
 
 
 date = datetime.now()
-BMI = float((wconverted) / (hconverted ** 2))   
+BMI = float(wconverted / (hconverted ** 2))
 print("Your BMI is: ", BMI, "as of ", date) 
-print("You are using a",os.name,"system")
+print("You are using a", os.name, "system")
