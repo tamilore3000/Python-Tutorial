aa8081a5ec2088ac4ec25b9735f394b7a1586757
olaoguntam9@gmail.com <Olaoguntam9@gmail.com>
2022-09-14 05:42:41 +0100

    added emoji project and others

---
+++ Datatypes/emojicon_project.py
@@ -0,0 +1,10 @@
+message = input("Enter message: ")
+words = message.split(" ")
+emojis = {
+    ":)" : "😃",
+    ":(" : "😔"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
