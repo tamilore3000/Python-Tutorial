359f53292d49c8431825808f8c4b6f96e2286ccc
Tamilore <olaoguntam9@gmail.com>
2023-04-23 21:33:35 +0100

    Added a folder for PROF EZE CLASS on modules

---
+++ EZE_CLASS/today/breath_in.py
@@ -0,0 +1,9 @@
+"""Programming is fun in Babcock"""
+
+
+chest = ["heart", "lungs", "ribs"]
+
+def breath_out():
+    pass
+
+happy = "I am a happy Nigerian , My name is Sansi Joshua with a Matric No: 19/2251"
